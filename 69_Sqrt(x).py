aa025e7401a743873a066d41142593e5862c9e0e
Chung Cơ Tuấn <chungcotuan11.3@gmail.com>
2026-04-03 08:33:21 +0700

    nghien cuu lai

---
+++ 69_Sqrt(x).py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x < 2:
+            return x
+        
+        left = 1
+        right = x // 2
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            sq = mid * mid
+            
+            if sq == x:
+                return mid
+            elif sq < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return right
