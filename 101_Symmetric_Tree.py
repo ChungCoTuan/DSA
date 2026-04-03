154ab32983c7a1286994e3daa1633950e9616518
Chung Cơ Tuấn <chungcotuan11.3@gmail.com>
2026-04-03 08:58:06 +0700

    .

---
+++ 101_Symmetric_Tree.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.isMirror(root.left, root.right)
+        
+    def isMirror(self, t1, t2):
+        if not t1 and not t2:
+            return True
+            
+        if not t1 or not t2:
+            return False
+            
+        if t1.val != t2.val:
+            return False
+            
+        return self.isMirror(t1.left, t2.right) and self.isMirror(t1.right, t2.left)
